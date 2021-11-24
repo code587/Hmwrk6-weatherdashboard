1a6a2b38f5b3b309abb5206ad7483d081864a273
Stephanie <sandora375@outlook.com>
2021-11-23 19:41:12 -0600

    added weather data to container with the current day information

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,27 @@
-
 let cityInput = document.querySelector("#city");
-let currentDayEl = document.querySelector("#currentday");
-let tempEl = document.querySelector("#temp");
-let windEl = document.querySelector("#wind");
-let humidityEl = document.querySelector("#humidity");
+currentDayEl = document.querySelector("#currentday");
+let tempEl = document.querySelector(".temp");
+let windEl = document.querySelector(".wind");
+let humidityEl = document.querySelector(".humidity");
 let uvIndexEl   = document.querySelector("#uvindex");
-let searchBtn = document.querySelector("#searchBtn");
+let searchBtn = document.querySelector("#searchbutton");
 
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-searchBtn.addEventListener("click", function(event){
+searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
     let citySearch = cityInput.value
     console.log(citySearch)
     getCurrentWeather(citySearch)
+
+    // let todayDate = moment()
+    // $("#currentDay").text(todayDate.format("M, D, YYYY"));
 })
    
-
 function getCurrentWeather(value) {
     console.log("cityInput value inside getcurrentweather", value)
-
+    console.log(value);
+    
     let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + "&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial";
-
     console.log(weatherUrl)
     
    fetch(weatherUrl)
@@ -33,38 +31,33 @@ function getCurrentWeather(value) {
         .then(function (weather) {
 
             console.log("GET CURRENT WEATHER",weather)
-    currentDayEl.textContent = weather.city.name;
-            tempEl.textContent = "Temperature: " +weather.list[0].main.temp + "F";
-            windEl.textContent = "Wind Speed: " + weather.list[0].wind.speed + "MPH";
+            currentDayEl.textContent = weather.city.name;
+            tempEl.textContent = "Temp: " +weather.list[0].main.temp + " F";
+            windEl.textContent = "Wind Speed: " + weather.list[0].wind.speed + " MPH";
             humidityEl.textContent = "Humidity: " + weather.list[0].main.humidity + "%";
-            
-            let coords = {
-               lat: weather.city.coord.lat,
-               lon: weather.city.coord.lon
-            }
+            getUVIndex(weather) 
+            })
+}
+function getUVIndex(weather){
+       
+    let latitude = weather.city.coord.lat 
+    let longitude = weather.city.coord.lon
+     
+    let coords = {lat: latitude, lon: longitude};
+    console.log(coords);
 
-            getUVIndex(coords)
-        })
- }
-        
- function getUVIndex(coords){
+    let oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&appid=78e465147d98ed2de5b5d10f98a4ff8f& units=imperial";
+    console.log(oneCallURL); 
 
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&units=imperial&appid=78e465147d98ed2de5b5d10f98a4ff8f"
-
-    fetch(weatherUrl)
+    fetch(oneCallURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log("GET UV INDEX",data)
- })
-
+        console.log("GET UV INDEX",data) // is giving me all the data still need to pluck out the uvi
+        uvIndexEl.textContent = "UV Index: " + weather.list[0].uvi;
+    })
 }
-
- 
-
-
-
 
 
 
