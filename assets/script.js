d9a92c23310ea8bd995d19dfd58bfca98fa9df5a
Stephanie <sandora375@outlook.com>
2021-12-08 23:27:28 -0600

    added current weather container

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,8 @@ searchBtn.addEventListener("click", function(event) {
     console.log(citySearch)
     getCurrentWeather(citySearch)
 
-    // let todayDate = moment()
-    // $("#currentDay").text(todayDate.format("M, D, YYYY"));
+    let todayDate = moment()
+    $("#currentDay").text(todayDate.format("M, D, YYYY"));
 })
    
 function getCurrentWeather(value) {
@@ -24,42 +24,40 @@ function getCurrentWeather(value) {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + "&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial";
     console.log(weatherUrl)
     
-   fetch(weatherUrl)
+   fetch(weatherUrl) 
         .then(function (response) {
             return response.json();
         })
         .then(function (weather) {
 
-            console.log("GET CURRENT WEATHER",weather)
             currentDayEl.textContent = weather.city.name;
             tempEl.textContent = "Temp: " +weather.list[0].main.temp + " F";
             windEl.textContent = "Wind Speed: " + weather.list[0].wind.speed + " MPH";
             humidityEl.textContent = "Humidity: " + weather.list[0].main.humidity + "%";
             getUVIndex(weather) 
+            
+            console.log("GET CURRENT WEATHER",weather)
             })
 }
-function getUVIndex(weather){
-       
+
+function getUVIndex(weather) {
+   
     let latitude = weather.city.coord.lat 
     let longitude = weather.city.coord.lon
-     
     let coords = {lat: latitude, lon: longitude};
     console.log(coords);
 
     let oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&appid=78e465147d98ed2de5b5d10f98a4ff8f& units=imperial";
     console.log(oneCallURL); 
-
-    fetch(oneCallURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log("GET UV INDEX",data) // is giving me all the data still need to pluck out the uvi
-        uvIndexEl.textContent = "UV Index: " + weather.list[0].uvi;
-    })
 }
 
+    // then(function (data) {
+    //     console.log("GET UV INDEX",data) // is giving me all the data still need to pluck out the uvi
+    
+    // })
 
+
+  
 
 //WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
