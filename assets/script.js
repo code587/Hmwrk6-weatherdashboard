e15e2095de1d8ad72fdd0ccec6533b1173ebd79e
Stephanie <sandora375@outlook.com>
2021-12-13 20:18:44 -0600

    added date to container

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,68 @@
 let cityInput = document.querySelector("#city");
-currentDayEl = document.querySelector("#currentday");
+let searchBtn = document.querySelector("#searchbutton");
+let cityNameEl = document.querySelector("#cityname");
+let todayEl = document.querySelector(".today");
 let tempEl = document.querySelector(".temp");
 let windEl = document.querySelector(".wind");
 let humidityEl = document.querySelector(".humidity");
 let uvIndexEl   = document.querySelector("#uvindex");
-let searchBtn = document.querySelector("#searchbutton");
+let weatherCards = document.querySelector(".card-deck");
 
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
     let citySearch = cityInput.value
     console.log(citySearch)
-    getCurrentWeather(citySearch)
+  
+    // let todayDate = moment()
+    // $("#today").text(today.format("dddd, MMMM D, YYYY"));
+    // console.log(todayDate);
+    // // let currentDate = moment().format("M, D, YYYY")
+    // // // $("currentDayEl").text(todayDate.format("M, D, YYYY"));
+    // // console.log(currentDate);
 
-    let todayDate = moment()
-    $("#currentDay").text(todayDate.format("M, D, YYYY"));
+    getCurrentWeather(citySearch)
+  
 })
    
 function getCurrentWeather(value) {
     console.log("cityInput value inside getcurrentweather", value)
     console.log(value);
     
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + "&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial";
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + "&cnt=1" + "&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial";
     console.log(weatherUrl)
-    
-   fetch(weatherUrl) 
+
+    fetch(weatherUrl) 
         .then(function (response) {
             return response.json();
         })
         .then(function (weather) {
-
-            currentDayEl.textContent = weather.city.name;
+            
+            cityNameEl.textContent = weather.city.name;
+            todayEl.textContent = weather.list[0].dt_txt; 
             tempEl.textContent = "Temp: " +weather.list[0].main.temp + " F";
             windEl.textContent = "Wind Speed: " + weather.list[0].wind.speed + " MPH";
             humidityEl.textContent = "Humidity: " + weather.list[0].main.humidity + "%";
-            getUVIndex(weather) 
-            
-            console.log("GET CURRENT WEATHER",weather)
-            })
+
+            console.log("GET CURRENT WEATHER",weather);
+            console.log(weather.city.coord);
+        })
 }
 
 function getUVIndex(weather) {
-   
+    let oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial"
+
+    console.log(oneCallURL);
+    // let oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&appid=78e465147d98ed2de5b5d10f98a4ff8f& units=imperial";
+    // console.log(oneCallURL);
     let latitude = weather.city.coord.lat 
     let longitude = weather.city.coord.lon
     let coords = {lat: latitude, lon: longitude};
     console.log(coords);
-
-    let oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lon + "&appid=78e465147d98ed2de5b5d10f98a4ff8f& units=imperial";
-    console.log(oneCallURL); 
 }
+
+
+
 
     // then(function (data) {
     //     console.log("GET UV INDEX",data) // is giving me all the data still need to pluck out the uvi
