0e1c0264ad50f1ecd90d4570ec1b9f32068b4645
Stephanie <sandora375@outlook.com>
2022-02-20 05:46:27 -0600

    icon image displays

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ let todayEl = document.querySelector(".today");
 let tempEl = document.querySelector(".temp");
 let windEl = document.querySelector(".wind");
 let humidityEl = document.querySelector(".humidity");
-// let uvInfoEl   = document.querySelector("#uvinfo");
 let todayEl2 = document.querySelector(".date");
 let tempEl2 = document.querySelector(".temp2");
 let windEl2 = document.querySelector(".wind2");
@@ -48,64 +47,68 @@ function priorSearch() {
         })
         .then(function (climate) {
             console.log(climate);
-            // console.log(data.city.coord.lat);
-            // console.log(data.city.coord.lon);
-            //console.log(data.list[0].weather.icon);
+            //console.log(climate.list[0].weather[0].icon);
             
             let lat = climate.city.coord.lat
             console.log(lat);
             let lon = climate.city.coord.lon
             console.log(lon);
-            // let imageEl = document.createElement("img")
-            // let iconImage = data.list[0].weather.icon
+            //let imageEl = document.createElement("img")
+            let iconEl = document.querySelector("#climateIcon");
+            let iconImage = climate.list[0].weather[0].icon
+            console.log(iconImage);
+            iconEl.src="http://openweathermap.org/img/wn/" + iconImage + ".png";
 
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&exclude={part}&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            //console.log(data.current.uvi);    
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&exclude={part}&appid=78e465147d98ed2de5b5d10f98a4ff8f&units=imperial")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (oneCallindex) {
+                //console.log(oneCallindex.current.uvi);    
+                
+                let uvResult = oneCallindex.current.uvi
+                console.log(uvResult);
+                
+                let uvInfoEl = document.querySelector(".uvInfo");
+                uvInfoEl.textContent = "UV Index: " + uvResult;
+                
+                let currentDate = moment().format("MM/DD/YYYY")
+                console.log(currentDate);
+                
 
-            let uvResult = data.current.uvi
-            console.log(uvResult);
 
-            let uvInfoEl = document.querySelector(".uvInfo");
-            uvInfoEl.textContent = "UV Index: " + uvResult;
-            
-            
 
-        
-        
-            cityNameEl.textContent = climate.city.name;
-            todayEl.textContent = climate.list[0].dt_txt; 
-            tempEl.textContent = "Temp: " + climate.list[0].main.temp + " F";
-            windEl.textContent = "Wind Speed: " + climate.list[0].wind.speed + " MPH";
+                
+                cityNameEl.textContent = climate.city.name;
+                todayEl.textContent = currentDate; 
+                tempEl.textContent = "Temp: " + climate.list[0].main.temp + " F";
+            windEl.textContent = "Wind: " + climate.list[0].wind.speed + " MPH";
             humidityEl.textContent = "Humidity: " + climate.list[0].main.humidity + "%";
             
-            // todayEl2.textContent = weather.list[5].dt_txt; 
-            // tempEl2.textContent = "Temp: " + weather.list[5].main.temp + " F";
-            // windEl2.textContent = "Wind Speed: " + weather.list[5].wind.speed + " MPH";
-            // humidityEl2.textContent = "Humidity: " + weather.list[5].main.humidity + "%";
+            todayEl2.textContent = climate.list[5].dt_txt; 
+            tempEl2.textContent = "Temp: " + climate.list[5].main.temp + " F";
+            windEl2.textContent = "Wind: " + climate.list[5].wind.speed + " MPH";
+            humidityEl2.textContent = "Humidity: " + climate.list[5].main.humidity + "%";
             
-            // todayEl3.textContent = weather.list[13].dt_txt; 
-            // tempEl3.textContent = "Temp: " + weather.list[13].main.temp + " F";
-            // windEl3.textContent = "Wind Speed: " + weather.list[13].wind.speed + " MPH";
-            // humidityEl3.textContent = "Humidity: " + weather.list[13].main.humidity + "%";
+            todayEl3.textContent = climate.list[13].dt_txt; 
+            tempEl3.textContent = "Temp: " + climate.list[13].main.temp + " F";
+            windEl3.textContent = "Wind: " + climate.list[13].wind.speed + " MPH";
+            humidityEl3.textContent = "Humidity: " + climate.list[13].main.humidity + "%";
             
-            // todayEl4.textContent = weather.list[21].dt_txt; 
-            // tempEl4.textContent = "Temp: " + weather.list[21].main.temp + " F";
-            // windEl4.textContent = "Wind Speed: " + weather.list[21].wind.speed + " MPH";
-            // humidityEl4.textContent = "Humidity: " + weather.list[21].main.humidity + "%";
+            todayEl4.textContent = climate.list[21].dt_txt; 
+            tempEl4.textContent = "Temp: " + climate.list[21].main.temp + " F";
+            windEl4.textContent = "Wind: " + climate.list[21].wind.speed + " MPH";
+            humidityEl4.textContent = "Humidity: " + climate.list[21].main.humidity + "%";
             
-            // todayEl5.textContent = weather.list[29].dt_txt; 
-            // tempEl5.textContent = "Temp: " + weather.list[29].main.temp + " F";
-            // windEl5.textContent = "Wind Speed: " + weather.list[29].wind.speed + " MPH";
-            // humidityEl5.textContent = "Humidity: " + weather.list[29].main.humidity + "%";
+            todayEl5.textContent = climate.list[29].dt_txt; 
+            tempEl5.textContent = "Temp: " + climate.list[29].main.temp + " F";
+            windEl5.textContent = "Wind: " + climate.list[29].wind.speed + " MPH";
+            humidityEl5.textContent = "Humidity: " + climate.list[29].main.humidity + "%";
             
-            // todayEl6.textContent = weather.list[37].dt_txt; 
-            // tempEl6.textContent = "Temp: " + weather.list[37].main.temp + " F";
-            // windEl6.textContent = "Wind Speed: " + weather.list[37].wind.speed + " MPH";
-            // humidityEl6.textContent = "Humidity: " + weather.list[37].main.humidity + "%";
+            todayEl6.textContent = climate.list[37].dt_txt; 
+            tempEl6.textContent = "Temp: " + climate.list[37].main.temp + " F";
+            windEl6.textContent = "Wind: " + climate.list[37].wind.speed + " MPH";
+            humidityEl6.textContent = "Humidity: " + climate.list[37].main.humidity + "%";
         })
     })
 }
@@ -116,10 +119,6 @@ function priorSearch() {
         console.log(citySearch)
         
         getExistingWeather(citySearch)
-
-        // let uvSearch = uvInput.value
-        // console.log(uvInput)
-        // getExistingUVIndex(uvSearch)
        
         localStorage.setItem("city", JSON.stringify(citySearch))
         prevCities.textContent = citySearch;
